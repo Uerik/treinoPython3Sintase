bc1cafea247851f88486b375df0b9e0bf98da1b0
Uerik <rodrigouerick.kbs@gmail.com>
2022-05-09 16:04:47 -0300

    Calculo de media

---
+++ mediaEscolar.py
@@ -0,0 +1,7 @@
+print('Bem-Vindo a media escolar!')
+n1 = float(input('Digite N1: '))
+n2 = float(input('Digite N2: '))
+n3 = float(input('Digite N3: '))
+n4 = float(input('Digite N4: '))
+media = (n1 + n2 + n3 + n4) / 4
+print('Media: ', media)
